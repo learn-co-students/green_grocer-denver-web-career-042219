365cd84c05f040c207ed484d6f1b85764e7ad135
Joe Tustin <j.tustin@gmail.com>
2019-03-29 19:13:21 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -3,6 +3,7 @@ require 'pry'
 
 #used a similar idea to what i learned on pigeon.  build the hash in steps.  since each key:value does not include an array
 #I could use two.  Also nested a count incrementer to catch them before
+
 def consolidate_cart(cart)
   cart_hash = Hash.new
 
@@ -26,10 +27,12 @@ def consolidate_cart(cart)
 return cart_hash
 end
 
-#I think consolidate the cart first, bash up coupons, return a new hash
 
+#I think consolidate the cart first, bash up coupons, return a new hash
+=begin
 require 'pry'
 def apply_coupons(cart, coupons)
+binding.pry
 
 cart_hash = Hash.new
 #consolidate_cart first
@@ -46,6 +49,7 @@ cart_hash = Hash.new
         if !cart_hash[item].has_key?(k)
           cart_hash[item][k] = v
         end
+
       end
     end
   end
@@ -65,36 +69,157 @@ cart_hash = Hash.new
         cart_coupon[item] = item_hash
       end
 
+
+    #  binding.pry
+
       if coupons_items.include?(item)
-        item_hash.each do |item_k, item_v|
-          coupons.each do |element|
-            element.each do |coupon_k, coupon_v|
-              if item_hash[:count] > element[:num]
+        coupons.find do |element| #using find here returns the correct price on the coupon item
+          if element[:item] == item
+            if item_hash[:count] >= element[:num] #check to see if there are more items than the coupon covers
+                item_count = item_hash[:count]
 
-              end 
+                cart_coupon["#{item} W/ COUPON"] = {}
+                cart_coupon["#{item} W/ COUPON"][:price] = element[:cost]
+                cart_coupon["#{item} W/ COUPON"][:clearance] = item_hash[:clearance]
+                cart_coupon["#{item} W/ COUPON"][:count] = 1
 
+                item_count =  item_count - element[:num]
 
+              if item_count > 0 # build a new key:value pair based on the remaining items
+                  cart_coupon[item] = {}
+                  cart_coupon[item][:price] = item_hash[:price]
+                  cart_coupon[item][:clearance] = item_hash[:clearance]
+                  cart_coupon[item][:count] = item_count
+                  item_count = 0
+                end
+              end
+            if item_hash[:count] < element[:num]
+              cart_coupon[item] = item_hash
+            end
 
-        binding.pry
+          end
+        end
       end
-      end
-      end
-      end
 
-    end
-puts cart_coupon
+    puts cart_coupon
+    return cart_coupon
+      end
 end
+=end
+
+#man that was a doozy, applied what I learned from other labs to create and modify the hashes.
+#a space in the w/coupon sent me on a goose chase for a while
+#interesting to think about how to structure and explore data structures, and what to plan for
+# created an array which took the couopon item name and stored in array, once a coupon was removed that value was deleted.
+# I think there is an easier way to do this but this does work, i could also just
+require 'pry'
+def apply_coupons(cart, coupons)
+
+  coupons_items = Array.new
+  original_cart = cart
+  original_coupons = coupons
+
+    coupons.each do |element| # grab each item name from the coupon hash
+      coupons_items << element[:item]
+    end
+
+    cart.each do |item, item_hash|
+      if !coupons_items.include?(item)
+        cart[item] = item_hash
+      end
+
+      if coupons_items.include?(item)
+        coupons.each do |element| #using find here returns the correct price on the coupon item
+
+         if element[:item] == item && coupons_items.include?(item)
+
+            if item_hash[:count] >= element[:num]
+              coupons_items.delete_at(coupons.index(element)) #check to see if there are more items than the coupon covers
+                item_count = item_hash[:count]
+                cart_coupon = Hash.new
+                cart_coupon["#{item} W/COUPON"] = {}
+                cart_coupon["#{item} W/COUPON"][:price] = element[:cost]
+                cart_coupon["#{item} W/COUPON"][:clearance] = item_hash[:clearance]
+                cart_coupon["#{item} W/COUPON"][:count] = 1
+                cart = cart.merge(cart_coupon)
+                item_count =  item_count - element[:num]
+            end
+
+          #    binding.pry
+            if item_count >= 0 # build a new key:value pair based on the remaining items and if the coupon array is empty
+                cart[item] = {}
+                cart[item][:price] = item_hash[:price]
+                cart[item][:clearance] = item_hash[:clearance]
+                cart[item][:count] = item_count
+            end
+
+            if cart[item][:count] > element[:num]  #I dont think this is the worst way but there is a better way
+              coupons_items.delete_at(coupons.index(element))
+              #could have recoreded how many coupons per food there were in the begining, then created
+              # the new hashes with that info
+              #modify the cart
+              #might not need to change the non coupon hash
+              cart["#{item} W/COUPON"][:count] = cart["#{item} W/COUPON"][:count] + 1
+              item_count = item_count - element[:num]
+              cart[item][:count] = cart[item][:count] - element[:num]
+              #binding.pry
+              #after this it goes back up to the top and resets the values
+            end
+
+          if item_hash[:count] < element[:num]
+            cart[item] = item_hash
+          end
+
+        end
+      end
+    end
+  end
+  return cart
+end
+
+
 
 def apply_clearance(cart)
-  # code here
+
+  discount = (1 - 0.2)
+  cart.each do |item, item_hash|
+    if item_hash[:clearance] == true
+      item_hash[:price] = (item_hash[:price] * discount).round(2)
+    end
+  end
 end
 
+
+
 def checkout(cart, coupons)
-  # code here
+
+  total = 0
+  discount = (1- 0.1)
+
+  consolidated = consolidate_cart(cart)
+  coupon_cart = apply_coupons(consolidated, coupons)
+  clearance_cart = apply_clearance(coupon_cart)
+
+
+    clearance_cart.each do |item|
+      item_total = (item[1][:price] * item[1][:count])
+      total = (total + item_total)
+    #  binding.pry
+    end
+
+    if total > 100
+      total = (total * discount).round(2)
+    end
+  #binding.pry
+  puts total
+  return total
+
+
 end
 
 
 =begin
+
 def cart
   [
     {"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
@@ -102,9 +227,6 @@ def cart
     {"AVOCADO" => {:price => 3.00, :clearance => true}},
     {"AVOCADO" => {:price => 3.00, :clearance => true}},
     {"KALE" => {:price => 3.00, :clearance => false}},
-    {"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
-    {"ALMONDS" => {:price => 9.00, :clearance => false}},
-    {"TEMPEH" => {:price => 3.00, :clearance => true}},
     {"CHEESE" => {:price => 6.50, :clearance => false}},
     {"BEER" => {:price => 13.00, :clearance => false}},
     {"BEETS" => {:price => 2.50, :clearance => false}},
@@ -113,7 +235,6 @@ def cart
   ]
 end
 
-
 def coupons
 [
 	{:item => "AVOCADO", :num => 2, :cost => 5.00},
@@ -122,38 +243,12 @@ def coupons
  ]
 end
 
-def cart_hash_3
-{
-  "AVOCADO"=>{:count=>3, :price=>3.0, :clearance=>true},
- "KALE"=>{:count=>2, :price=>3.0, :clearance=>false},
- "BLACK_BEANS"=>{:count=>1, :price=>2.5, :clearance=>false},
- "ALMONDS"=>{:count=>1, :price=>9.0, :clearance=>false},
- "TEMPEH"=>{:count=>1, :price=>3.0, :clearance=>true},
- "CHEESE"=>{:count=>1, :price=>6.5, :clearance=>false},
- "BEER"=>{:count=>1, :price=>13.0, :clearance=>false},
- "PEANUTBUTTER"=>{:count=>1, :price=>3.0, :clearance=>true},
- "BEETS"=>{:count=>1, :price=>2.5, :clearance=>false},
- "SOY MILK"=>{:count=>1, :price=>4.5, :clearance=>true}
-}
-end
 
-
-
-
-
-def cart
+def coupons
 [
-  {"KALE" => {:price => 3.00, :clearance => false}},
-  {"KALE" => {:price => 3.00, :clearance => false}}
+  {:item=>"AVOCADO", :num=>2, :cost=>5.0, :place=>1},
+  {:item=>"AVOCADO", :num=>2, :cost=>5.0, :place=>2}
 ]
-end
-
-def coupons
-[
-	{:item => "AVOCADO", :num => 2, :cost => 5.00},
-	{:item => "BEER", :num => 2, :cost => 20.00},
-	{:item => "CHEESE", :num => 3, :cost => 15.00}
- ]
 end
 
 
